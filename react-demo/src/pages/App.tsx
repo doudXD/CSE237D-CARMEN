9a4dbee5aa5dc1c77c9cddf4670a25981dde88fc
Dou <doud.xie@gmail.com>
2024-05-24 23:08:57 -0700

    some clean up and document

--- react-demo/src/pages/App.tsx
+++ react-demo/src/pages/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-// import useWebSocket from "react-use-websocket";
-// import { ReadyState } from "react-use-websocket"; // Import the ReadyState type
 
 //Import Custom Components
 import { PromptOptions } from "../containers/PromptOptions";
@@ -8,9 +6,6 @@ import { AnimationOptions } from "../containers/AnimationOptions";
 import { Button } from "../components/Button";
 import { useLocation } from "react-router-dom";
 
-//URLs for mock server and bot connection
-// const LOCAL_URL = "ws://127.0.0.1:800";
-// const BOT_URL = "ws://100.84.29.19:5000";
 interface LocationState {
   roboturl: string;
   token: string;
@@ -20,7 +15,11 @@ import "./App.css";
  * Base Container of Web App
  */
 function App() {
-  //setup websocket values
+  // URLs for mock server and bot connection
+  // const LOCAL_URL = "ws://127.0.0.1:800";
+  // const BOT_URL = "ws://100.84.29.19:5000";
+
+  //Get URL and token from previous page
   const location = useLocation();
   const { roboturl = "ws://100.84.29.19:5000", token } =
     (location.state as LocationState) || {};
